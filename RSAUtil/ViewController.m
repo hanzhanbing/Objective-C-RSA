c5cc6b6f780076a26bf6903b4c19357d8950932e
hanzhanbing <1655661337@qq.com>
2018-12-19 10:06:28 +0800

    RSA加密解密测试

--- RSAUtil/ViewController.m
+++ RSAUtil/ViewController.m
@@ -18,40 +18,19 @@
 - (void)viewDidLoad {
 	[super viewDidLoad];
 
+	NSString *pubkey = @"MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCPegU1e8FZrnuKJKUF69YmRfcZdckB4F/KWrt7KCeMnv87MKGGsjF3WeUvp3m3vCzmHNoUJgQyWCfsj4DHWUCjVnJVWuPs5Xr5tBJh8hq/wVtVQ7dYcwYVt0CrZK6D2nb4LDa9Cbh54cZCQ7vlNxWTVoVnRH758LYgIs9l2WtqYwIDAQAB";
+    
+	NSString *privkey = @"MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAI96BTV7wVmue4okpQXr1iZF9xl1yQHgX8pau3soJ4ye/zswoYayMXdZ5S+nebe8LOYc2hQmBDJYJ+yPgMdZQKNWclVa4+zlevm0EmHyGr/BW1VDt1hzBhW3QKtkroPadvgsNr0JuHnhxkJDu+U3FZNWhWdEfvnwtiAiz2XZa2pjAgMBAAECgYAqwzZ8O1dkcGG3ybY5rpHUNHQybW2sloBWYnaQfWZUPitaK+NorwGZTfzGGEdp4LcNGrpQ2QfVLXswwZCjWdGuphLhIj1+ryXxnN0ggdEanAD4qAW4mbAt0HDD54MF/7bwv/wtr3CNEJjw9Bkmmo4Akyhwruepg9jJzJP7CYafEQJBAP/Kye3pWSsEBlRctQ67aYVJeJQOjtYyAzTMQwJOvaWi9d2dU0f0X+6xApHateHzJC4TtU3mXEZKqjNfDF3C9YkCQQCPl936ntlW0wzDRDYsrfROFTwZHlqB6VmTUIgzuxBqhjtVExgBqvvNwchMZfdILYUsi83wqu6++qlpNbfdshGLAkEApTb8OljQKUKOPAstPvi57UdJRIgqphrji0Sru1/Uipm5KI0nb4BePjPCryaO+AQtyD0ei9RQoW40hL3NpPn50QJAD2V2PLAZl7Crdqhyspm4T0MBnaPein1tyFjjMcDGAtX/uKCCe3NM6oPzqRVpzUhsNhmLbFm3naycy5Yh79hu/wJAfW4MSXWPgv1CpRfgyLZKI89cf23EHUv68cZNf/9tR88QpOAb8I9rocJDzc6dyRSdpAVpay4LsXzTz7E1VRKROQ==";
 
-	NSString *pubkey = @"-----BEGIN PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDDI2bvVLVYrb4B0raZgFP60VXY\ncvRmk9q56QiTmEm9HXlSPq1zyhyPQHGti5FokYJMzNcKm0bwL1q6ioJuD4EFI56D\na+70XdRz1CjQPQE3yXrXXVvOsmq9LsdxTFWsVBTehdCmrapKZVVx6PKl7myh0cfX\nQmyveT/eqyZK1gYjvQIDAQAB\n-----END PUBLIC KEY-----";
-	NSString *privkey = @"-----BEGIN PRIVATE KEY-----\nMIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAMMjZu9UtVitvgHS\ntpmAU/rRVdhy9GaT2rnpCJOYSb0deVI+rXPKHI9Aca2LkWiRgkzM1wqbRvAvWrqK\ngm4PgQUjnoNr7vRd1HPUKNA9ATfJetddW86yar0ux3FMVaxUFN6F0KatqkplVXHo\n8qXubKHRx9dCbK95P96rJkrWBiO9AgMBAAECgYBO1UKEdYg9pxMX0XSLVtiWf3Na\n2jX6Ksk2Sfp5BhDkIcAdhcy09nXLOZGzNqsrv30QYcCOPGTQK5FPwx0mMYVBRAdo\nOLYp7NzxW/File//169O3ZFpkZ7MF0I2oQcNGTpMCUpaY6xMmxqN22INgi8SHp3w\nVU+2bRMLDXEc/MOmAQJBAP+Sv6JdkrY+7WGuQN5O5PjsB15lOGcr4vcfz4vAQ/uy\nEGYZh6IO2Eu0lW6sw2x6uRg0c6hMiFEJcO89qlH/B10CQQDDdtGrzXWVG457vA27\nkpduDpM6BQWTX6wYV9zRlcYYMFHwAQkE0BTvIYde2il6DKGyzokgI6zQyhgtRJ1x\nL6fhAkB9NvvW4/uWeLw7CHHVuVersZBmqjb5LWJU62v3L2rfbT1lmIqAVr+YT9CK\n2fAhPPtkpYYo5d4/vd1sCY1iAQ4tAkEAm2yPrJzjMn2G/ry57rzRzKGqUChOFrGs\nlm7HF6CQtAs4HC+2jC0peDyg97th37rLmPLB9txnPl50ewpkZuwOAQJBAM/eJnFw\nF5QAcL4CYDbfBKocx82VX/pFXng50T7FODiWbbL4UnxICE0UBFInNNiWJxNEb6jL\n5xd0pcy9O2DOeso=\n-----END PRIVATE KEY-----";
-
-	NSString *originString = @"hello world!";
-	for(int i=0; i<4; i++){
-		originString = [originString stringByAppendingFormat:@" %@", originString];
-	}
-	NSString *encWithPubKey;
-	NSString *decWithPrivKey;
-	NSString *encWithPrivKey;
-	NSString *decWithPublicKey;
+	NSString *originString = @"xlgcx";
 	
 	NSLog(@"Original string(%d): %@", (int)originString.length, originString);
 	
-	// Demo: encrypt with public key
-	encWithPubKey = [RSA encryptString:originString publicKey:pubkey];
+	NSString *encWithPubKey = [RSA encryptString:originString publicKey:pubkey];
 	NSLog(@"Enctypted with public key: %@", encWithPubKey);
-	// Demo: decrypt with private key
-	decWithPrivKey = [RSA decryptString:encWithPubKey privateKey:privkey];
-	NSLog(@"Decrypted with private key: %@", decWithPrivKey);
-	
-	// by PHP
-	encWithPubKey = @"CKiZsP8wfKlELNfWNC2G4iLv0RtwmGeHgzHec6aor4HnuOMcYVkxRovNj2r0Iu3ybPxKwiH2EswgBWsi65FOzQJa01uDVcJImU5vLrx1ihJ/PADUVxAMFjVzA3+Clbr2fwyJXW6dbbbymupYpkxRSfF5Gq9KyT+tsAhiSNfU6akgNGh4DENoA2AoKoWhpMEawyIubBSsTdFXtsHK0Ze0Cyde7oI2oh8ePOVHRuce6xYELYzmZY5yhSUoEb4+/44fbVouOCTl66ppUgnR5KjmIvBVEJLBq0SgoZfrGiA3cB08q4hb5EJRW72yPPQNqJxcQTPs8SxXa9js8ZryeSxyrw==";
-	decWithPrivKey = [RSA decryptString:encWithPubKey privateKey:privkey];
-	NSLog(@"(PHP enc)Decrypted with private key: %@", decWithPrivKey);
-	
-	// Demo: encrypt with private key
-	encWithPrivKey = [RSA encryptString:originString privateKey:privkey];
-	NSLog(@"Enctypted with private key: %@", encWithPrivKey);
 
-	// Demo: decrypt with public key
-	decWithPublicKey = [RSA decryptString:encWithPrivKey publicKey:pubkey];
-	NSLog(@"(PHP enc)Decrypted with public key: %@", decWithPublicKey);
+	NSString *decWithPrivKey = [RSA decryptString:encWithPubKey privateKey:privkey];
+	NSLog(@"Decrypted with private key: %@", decWithPrivKey);
 }
 
 @end
